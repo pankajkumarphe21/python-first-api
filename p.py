63dd2c4fe68f4806cd6fa7a7e2b94807e0c4756e
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-12-04 02:41:35 +0530

    fifth commit

--- p.py
+++ p.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return jsonify({"message": "Welcome to Vercel Deployed API!"})
+
+@app.route("/user/<userId>")
+def getUser(userId):
+    return jsonify(f"Hi {userId}")
 
 if __name__ == "__main__":
     app.run()
